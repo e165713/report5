bc5b0e94361ae848b47fafaedd46249e503c555c
Takahiro Ikki <e165713@ie.u-ryukyu.ac.jp>
2016-12-12 14:48:52 +0900

    レポート課題

---
+++ src/main/jp/ac/uryukyu/ie/e165713/main.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e165713;
+
+/**
+ * Created by ichikitakahiro on 2016/12/12.
+ */
+public class main {
+    public static void  main(String[] args){
+        String str = null;
+        int length = str.length();
+        System.out.print(length);
+    }
+}
