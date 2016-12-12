d4b9f98bbbf4f0969bbe962d35b699a1a9453e64
Takahiro Ikki <e165713@ie.u-ryukyu.ac.jp>
2016-12-12 17:50:54 +0900

    レポート課題

--- src/main/jp/ac/uryukyu/ie/e165713/main.java
+++ src/main/jp/ac/uryukyu/ie/e165713/main.java
@@ -6,7 +6,12 @@ package jp.ac.uryukyu.ie.e165713;
 public class main {
     public static void  main(String[] args){
         String str = null;
-        int length = str.length();
-        System.out.print(length);
+        try {
+            int length = str.length();
+            System.out.print(length);
+        }catch(NullPointerException e){
+            System.out.printf("標準エラー出力%sが発生",e.toString());
+            e.printStackTrace();
+        }
     }
 }
