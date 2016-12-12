6c0dca7e01f3b67e097631d3ed73f7bb9aef714c
Takahiro Ikki <e165713@ie.u-ryukyu.ac.jp>
2016-12-12 18:04:10 +0900

    レポート課題

--- src/main/jp/ac/uryukyu/ie/e165713/main.java
+++ src/main/jp/ac/uryukyu/ie/e165713/main.java
@@ -1,14 +1,18 @@
 package jp.ac.uryukyu.ie.e165713;
 
+import java.util.NavigableMap;
+
 /**
  * Created by ichikitakahiro on 2016/12/12.
  */
 public class main {
     public static void  main(String[] args){
-        String str = null;
+        String str = "3．14";
+        double value;
         try {
             int length = str.length();
             System.out.print(length);
+            value = Double.parseDouble(str);
         }catch(NullPointerException e){
             System.out.printf("標準エラー出力%sが発生",e.toString());
             e.printStackTrace();
